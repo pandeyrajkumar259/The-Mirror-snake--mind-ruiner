658d7550a9455bd382b4ab581853a22705df4078
Raj Kumar Pandey <pandeyrajkumar259@gmail.com>
2026-06-20 12:45:38 +0000

    ---------------------

--- snake-game.js
+++ snake-game.js
@@ -206,9 +206,9 @@ function startGame(){
         }
 
         for(let i=1; i<redSnake.length;i++){
-         if(bHead.x===redSnake[i].x && bHead.y===redSnake[i].y {
-         endGame("Blue bited Red!🫠");
-         return;
+         if(bHead.x===redSnake[i].x && bHead.y===redSnake[i].y{
+          endGame("Blue bited Red!🫠");
+          return;
          }
         }
 
